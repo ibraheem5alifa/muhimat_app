0fcf5acb2790d609224cd7adef481a60ed54be4b
ibraheem5alifa <ibraheem5alifa@gmail.com>
2025-03-18 05:52:48 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/muhimat_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.muhimat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MuhimatApp());
+}
+
+class MuhimatApp extends StatelessWidget {
+  const MuhimatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
