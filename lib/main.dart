286808798c3edbe2b86167301f3112964524e9d9
ibraheem5alifa <ibraheem5alifa@gmail.com>
2025-03-18 06:07:31 +0200

    add project file model ,view and widgets create home screen file create custom text widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:muhimat_app/views/home_screen.dart';
 
 void main() {
   runApp(MuhimatApp());
@@ -9,6 +10,10 @@ class MuhimatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      // theme: ThemeData(brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
---
+++ lib/widgets/custom_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  const CustomText({
+    super.key,
+    required this.txt,
+    this.fontWeight = FontWeight.bold,
+    this.fontsize = 20,
+    this.color = Colors.black,
+  });
+  final String txt;
+  final FontWeight fontWeight;
+  final double fontsize;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      txt,
+      style: TextStyle(
+        fontWeight: fontWeight,
+        fontSize: fontsize,
+        color: color,
+      ),
+    );
+  }
+}
